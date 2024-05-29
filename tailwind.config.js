688ced5aec61c70808bdee9c347c8444690d075b
Martin Sanchez <118947989+dualipin@users.noreply.github.com>
2024-05-29 16:56:29 -0600

    feat: add new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: "#E6DDBC",
+          200: "#c8bf9f",
+          300: "#847c5e",
+        },
+        accent: {
+          100: "#822626",
+          200: "#ffb4a9",
+        },
+        color: {
+          100: "#525252",
+          200: "#7e7e7e",
+        },
+        dark: {
+          100: "#262525",
+          200: "#353434",
+          300: "#4e4d4d",
+        },
+      },
+    },
   },
   plugins: [],
 };
