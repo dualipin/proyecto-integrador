9163939fc3c5e6847eb130553b75b8d9e8cee57e
Martin Sanchez <118947989+dualipin@users.noreply.github.com>
2024-05-30 11:02:34 -0600

    feat: create Main Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
+import { Link, Outlet } from "react-router-dom";
+import NavigationJuegos from "./components/NavigationJuegos";
+
 function App() {
-  return <></>;
+  return (
+    <div className="flex flex-col min-h-screen bg-dark-100 text-color-200 overflow-hidden">
+      <header className="animate__animated animate__fadeInDown flex justify-between items-center bg-dark-200 m-2 lg:mx-10 rounded-xl shadow-md px-5 md:px-8 py-3 ">
+        <h1 className="text-center text-lg md:text-2xl font-semibold">
+          <Link to="/">El Azar</Link>
+        </h1>
+        <NavigationJuegos />
+      </header>
+      <Outlet />
+      <footer className="animate__animated animate__fadeInUp mt-auto text-center text-sm py-2 bg-dark-200 rounded-t-2xl shadow-md text-color-200">
+        Hecho con mucho ❤️ por Martin Sanchez -{" "}
+        <a
+          className="font-medium italic hover:underline"
+          target="_blank"
+          href="https://github.com/dualipin"
+        >
+          dualipin
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import reactRouter from "./Router.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import "animate.css";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={reactRouter} />
+  </React.StrictMode>
+);
