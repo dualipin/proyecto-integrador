c5f21c12a478947f4faa94f8cf6ee8330ed4bcce
Martin Sanchez <118947989+dualipin@users.noreply.github.com>
2024-06-01 11:56:34 -0600

    fix: change color hover of items menu

--- src/components/NavigationJuegos.tsx
+++ src/components/NavigationJuegos.tsx
@@ -13,14 +13,14 @@ function NavigationJuegos() {
     matchHome?.pathname != "/" && (
       <nav className="flex">
         <ul className="flex gap-4 justify-center items-center">
-          <li className="inline-flex items-center gap-2 rounded-md hover:bg-primary-300 bg-dark-100 py-1.5 px-3 text-sm/6 font-semibold text-color-200 hover:text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-primary-300 data-[open]:bg-dark-300 data-[focus]:outline-1 data-[focus]:outline-white">
+          <li className="inline-flex items-center gap-2 rounded-md bg-dark-100 py-1.5 px-3 text-sm/6 font-semibold text-color-200 hover:bg-dark-300 shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-primary-300 data-[open]:bg-dark-300 data-[focus]:outline-1 data-[focus]:outline-white">
             <a className="" href="/">
               Inicio
             </a>
           </li>
           <li>
             <Menu>
-              <MenuButton className="inline-flex items-center gap-2 rounded-md bg-dark-100 py-1.5 px-3 text-sm/6 font-semibold text-color-200 hover:text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-primary-300 data-[open]:bg-dark-300 data-[focus]:outline-1 data-[focus]:outline-white">
+              <MenuButton className="inline-flex items-center gap-2 rounded-md bg-dark-100 py-1.5 px-3 text-sm/6 font-semibold text-color-200 hover:text-accent-100 shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-primary-300 data-[open]:bg-dark-300 data-[focus]:outline-1 data-[focus]:outline-white">
                 Juegos
                 <svg
                   className="-mr-1 h-5 w-5 text-gray-400"
@@ -29,9 +29,9 @@ function NavigationJuegos() {
                   aria-hidden="true"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                   />
                 </svg>
               </MenuButton>
@@ -45,12 +45,12 @@ function NavigationJuegos() {
               >
                 <MenuItems
                   anchor="bottom end"
-                  className="mt-1 w-52 origin-top-right rounded-xl border backdrop-blur-sm border-white/5 bg-white/5 p-1 text-sm/6 text-white [--anchor-gap:var(--spacing-1)] focus:outline-none"
+                  className="mt-1 w-52 origin-top-right rounded-xl border backdrop-blur-sm border-white/5 bg-dark-300/60 p-1 text-sm/6 text-color-200 [--anchor-gap:var(--spacing-1)] focus:outline-none"
                 >
                   <MenuItem>
                     <Link
                       to={"/dados"}
-                      className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10"
+                      className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/40"
                     >
                       Dados
                     </Link>
@@ -58,7 +58,7 @@ function NavigationJuegos() {
                   <MenuItem>
                     <Link
                       to={"/gato"}
-                      className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10"
+                      className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/40"
                     >
                       Gato - Tres en raya
                     </Link>
@@ -66,7 +66,7 @@ function NavigationJuegos() {
                   <MenuItem>
                     <Link
                       to={"/memorama"}
-                      className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10"
+                      className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/40"
                     >
                       Memorama
                     </Link>
@@ -74,7 +74,7 @@ function NavigationJuegos() {
                   <MenuItem>
                     <Link
                       to={"/adivina"}
-                      className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10"
+                      className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/40"
                     >
                       Adivina el numero
                     </Link>
