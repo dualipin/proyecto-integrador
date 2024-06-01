f60ad9b17613c687746d40c98f7940a597dd1d0c
Martin Sanchez <118947989+dualipin@users.noreply.github.com>
2024-06-01 11:58:15 -0600

    feat: add transition

--- src/layout/JuegosLayout.tsx
+++ src/layout/JuegosLayout.tsx
@@ -5,7 +5,7 @@ type JuegosLayoutProps = {
 
 function JuegosLayout({ children, title }: JuegosLayoutProps) {
   return (
-    <main className="flex flex-col flex-1 p-5 gap-8">
+    <main className="animate__animated animate__fadeIn flex flex-col flex-1 p-5 gap-8">
       <h2 className="text-center font-semibold text-2xl">{title}</h2>
       <section className="flex-1 flex flex-col mt">{children}</section>
     </main>
